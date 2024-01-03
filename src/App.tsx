2822e129e2a2a07d105267c2bc1f2d9d8b6accb5
chengdai <chengdai.dev@gmail.com>
2024-01-03 15:44:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Lizhizhuangbi</h1>
+      <a href="https://m.weibo.cn/u/5976663270?t=&luicode=10000011&lfid=100103type%3D1%26q%3Dasdzxc" target="_blank">weibo</a>
+      
+     
+    </>
+  )
+}
+
+export default App
